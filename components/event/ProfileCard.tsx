27e73ee600644e5612499033bc9dee24f188e706
unknown <rana61960@gmail.com>
2024-10-04 15:13:48 +0600

    profile page maybe done if any error u can know me later

--- components/event/ProfileCard.tsx
+++ components/event/ProfileCard.tsx
@@ -4,27 +4,59 @@ import img2 from "./profilecardassets/Frame 1618872999 (1).png";
 import img3 from "./profilecardassets/Frame 1618872999 (2).png";
 import img4 from "./profilecardassets/Frame 1618872999 (3).png";
 import img5 from "./profilecardassets/Frame 1618872999.png";
-import img7 from "./profilecardassets/web-design-02.png";
 
-// type AppProfile = {
-//   description: string;
-//   imgUrl: any;
-// };
-const ProfileCard = () => {
+type ProfileCardProps = {
+  phoneNumber: string;
+  email: string;
+  location: string;
+  username: string;
+  website: string;
+};
+
+const ProfileCard = ({
+  phoneNumber,
+  email,
+  location,
+  username,
+  website,
+}: ProfileCardProps) => {
+  const profileIcon = [
+    {
+      imgUrl: img1,
+      description: phoneNumber,
+    },
+    {
+      imgUrl: img2,
+      description: email,
+    },
+    {
+      imgUrl: img3,
+      description: location,
+    },
+    {
+      imgUrl: img4,
+      description: website,
+    },
+    {
+      imgUrl: img5,
+      description: username,
+    },
+  ];
+
   return (
-    <div className="bg-white w-auto rounded-xl shadow-xl p-5 h-[300px] ">
+    <div className="bg-white w-full rounded-xl shadow-xl p-4  h-auto ">
       <div className="flex items-center justify-center gap-2 mb-4">
         <Image width={40} height={40} src="/assets/elements.png" alt="" />
-        <p className="text-[12px] w-full">Contact Details</p>
+        <p className=" w-full">Contact Details</p>
       </div>
       {profileIcon.map((items) => (
         <div
           key={items.description}
           className="flex items-center justify-center"
         >
-          <div className="w-[200px] flex items-center justify-start gap-1 mb-5">
+          <div className="w-full flex justify-start gap-1 mb-5">
             <Image src={items.imgUrl} alt="Icon" className="w-[20px]" />
-            <div className="text-[16px] text-[#64748B]">
+            <div className="text-[16px] text-[#64748B] break-all">
               {items.description}
             </div>
           </div>
@@ -35,26 +67,3 @@ const ProfileCard = () => {
 };
 
 export default ProfileCard;
-
-const profileIcon = [
-  {
-    imgUrl: img1,
-    description: "8801629897313",
-  },
-  {
-    imgUrl: img2,
-    description: "Mdrak@gmail.com",
-  },
-  {
-    imgUrl: img3,
-    description: "Dhaka, Bangladesh",
-  },
-  {
-    imgUrl: img4,
-    description: "ui/uxdesigner.com",
-  },
-  {
-    imgUrl: img5,
-    description: "Username",
-  },
-];
